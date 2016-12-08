c44ab8edd7d8a9b1cc15cf9d51f33dea447cdd48
Antoine Pingault <antoine.pingault@gmail.com>
2016-12-08 17:54:58 +0100

    First commit of old Doc version in markdown + script to create latex/pdf version of it

---
+++ docs/manual/generatePDFDoc.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Latex/pdf doc is generated with pandoc, see http://pandoc.org/MANUAL.html for more info
+for file in install devguide deploy userguide
+  do
+    pandoc ${file}.md -f markdown -t latex --smart --toc --normalize --number-sections -o ${file}.pdf ${file}.yaml -->
+done
