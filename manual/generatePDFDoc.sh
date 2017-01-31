f730f58e8c307065e3937a2ed41d4da316cc1f04
Remi Ete <remi.ete@gmail.com>
2017-01-31 19:05:38 +0000

    Deployed 47cea05 with MkDocs version: 0.16.0

--- manual/generatePDFDoc.sh
+++ manual/generatePDFDoc.sh
@@ -3,5 +3,5 @@
 # Latex/pdf doc is generated with pandoc, see http://pandoc.org/MANUAL.html for more info
 for file in install devguide deploy userguide
   do
-    pandoc ${file}.md -f markdown -t latex --smart --toc --normalize --number-sections -o ${file}.pdf ${file}.yaml -->
+    pandoc ${file}.md -f markdown -t latex --smart --toc --normalize --number-sections -o ${file}.pdf ${file}.yaml
 done
